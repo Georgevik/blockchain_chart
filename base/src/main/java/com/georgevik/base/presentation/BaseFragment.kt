3166a04d1ac6a07477aac6b771dd70f940cf37f1
Georgevik <jorgevik@gmail.com>
2019-05-20 01:32:58 +0200

    - Remain just injection staff in BaseFragmentInjector.kt

--- base/src/main/java/com/georgevik/base/presentation/BaseFragment.kt
+++ base/src/main/java/com/georgevik/base/presentation/BaseFragment.kt
@@ -1,11 +1,26 @@
 package com.georgevik.base.presentation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.CallSuper
+import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import javax.inject.Inject
 
 abstract class BaseFragment : Fragment() {
+
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    @CallSuper
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(getLayoutId(), container, false)
+    }
+
+    @LayoutRes
+    protected abstract fun getLayoutId(): Int
 
 }
--- base/src/main/java/com/georgevik/base/presentation/BaseFragmentInjector.kt
+++ base/src/main/java/com/georgevik/base/presentation/BaseFragmentInjector.kt
@@ -1,23 +1,12 @@
 package com.georgevik.base.presentation
 
 import android.content.Context
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.annotation.CallSuper
-import androidx.annotation.LayoutRes
 
 abstract class BaseFragmentInjector<in PROVIDER> : BaseFragment() {
     override fun onAttach(context: Context) {
         onInject(createInjector())
 
         super.onAttach(context)
-    }
-
-    @CallSuper
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(getLayoutId(), container, false)
     }
 
     @Suppress("UNCHECKED_CAST")
@@ -28,6 +17,5 @@ abstract class BaseFragmentInjector<in PROVIDER> : BaseFragment() {
 
     abstract fun onInject(provider: PROVIDER)
 
-    @LayoutRes
-    protected abstract fun getLayoutId(): Int
+
 }
