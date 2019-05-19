0ba8b54d44fce15edb9ca0b6d492730a90b4c3e1
Georgevik <jorgevik@gmail.com>
2019-05-19 10:43:25 +0200

    - Create RequestMarketPriceUseCase.kt

---
+++ pricechart/src/main/java/com/georgevik/pricechart/domain/SampleInterval.kt
@@ -0,0 +1,11 @@
+package com.georgevik.pricechart.domain
+
+enum class SampleInterval {
+    ONE_WEEK,
+    TWO_WEEKS,
+    THREE_WEEKS,
+    FIVE_WEEKS,
+    ONE_MONTH,
+    THREE_MONTHS,
+    ONE_YEAR
+}
